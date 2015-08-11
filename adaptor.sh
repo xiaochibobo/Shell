9290e8464a8996f813ee88ff946ff3684f63f5c2
root <root@www.liuchi.com>
2015-08-12 00:42:16 +0800

    速率计算问题解决

--- adaptor.sh
+++ adaptor.sh
@@ -7,8 +7,8 @@ txbefore=`ifconfig eth0 | sed -n "8p" | awk '{print $6}' | cut -c7-`
 sleep 2
 rxafter=`ifconfig eth0 | sed -n "8p" | awk '{print $2}' | cut -c7-`
 txafter=`ifconfig eth0 | sed -n "8p" | awk '{print $6}' | cut -c7-`
-rxresult=$[(rxafter-rxbefore)/16]
-txresult=$[(txafter-txbefore)/16]
+rxresult=$[(rxafter-rxbefore)/256] #首先转换成b,需要乘8,然后除以2,再除以1024
+txresult=$[(txafter-txbefore)/256] 
 echo "$time Now_in_speed:"$rxresult"Kbps Now_out_speed:"$txresult"Kbps"
 sleep 2
 done
