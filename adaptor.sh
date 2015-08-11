cebcd24cad92a60901ac4985135db4d116c3ad1f
root <root@www.liuchi.com>
2015-08-12 00:33:18 +0800

    速率问题没搞定

--- adaptor.sh
+++ adaptor.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+#查看主机网卡流量
+while :; do
+time=`date "+%X"`
+rxbefore=`ifconfig eth0 | sed -n "8p" | awk '{print $2}' | cut -c7-`
+txbefore=`ifconfig eth0 | sed -n "8p" | awk '{print $6}' | cut -c7-`
+sleep 2
+rxafter=`ifconfig eth0 | sed -n "8p" | awk '{print $2}' | cut -c7-`
+txafter=`ifconfig eth0 | sed -n "8p" | awk '{print $6}' | cut -c7-`
+rxresult=$[(rxafter-rxbefore)/16]
+txresult=$[(txafter-txbefore)/16]
+echo "$time Now_in_speed:"$rxresult"Kbps Now_out_speed:"$txresult"Kbps"
+sleep 2
+done
