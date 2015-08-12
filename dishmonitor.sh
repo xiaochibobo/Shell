42d2fbfe16746a2c6fe0f9feae4024d2d98f27e6
root <root@www.liuchi.com>
2015-08-12 10:06:09 +0800

    简化代码，结合了awk

--- dishmonitor.sh
+++ dishmonitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #监控主机的磁盘空间,当使用空间超过90％就通过发mail来发警告
 space=`df | sed 's/%//' | awk 'BEGIN{getline;sum=0} {sum+=$5} END{print sum}'`
-if [ $space -gt 20 ]
+if [ $space -gt 90 ]
 then
 	echo "Disk Full" | mail -s "Your disk is full" root@localhost 
 fi
